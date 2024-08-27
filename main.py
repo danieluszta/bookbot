8b819ecbcfe6e5ed08b87a254f5fad07bc2c3c79
usda <daniel.uszta@gmail.com>
2024-08-27 12:04:35 +0200

    added function to count words and count # of instances of words

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import string
 def main():
     path = "./books/frankenstein.txt"
-    read_text(path)
+    print(read_text(path))
+    print(count_words(read_text(path)))
+    print(word_instances(read_text(path)))
 
 def read_text(path):
     with open(path) as f:
-      return  print(f.read())
+      return f.read()
 
+def count_words(text):
+    new_list = text.split()
+    return len(new_list)
 
+def word_instances(text):
+    instances_dict = {}
+    alphabet = list(string.ascii_lowercase)
+    for letter in alphabet:
+        instances_dict[letter] = text.lower().count(letter)
+    return instances_dict
 
 
 
