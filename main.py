e6c23ba012df387acde358c68fc92dea3f782853
usda <daniel.uszta@gmail.com>
2024-08-27 13:04:13 +0200

    added a report with the count of words and count of instances of characters

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import string
 def main():
     path = "./books/frankenstein.txt"
-    print(read_text(path))
-    print(count_words(read_text(path)))
-    print(word_instances(read_text(path)))
+    red_text = read_text(path)
+    word_count = count_words(red_text)
+    count_letter_instances = word_instances(red_text)
+    report(word_count,count_letter_instances)
 
 def read_text(path):
     with open(path) as f:
@@ -12,13 +13,24 @@ def read_text(path):
 def count_words(text):
     new_list = text.split()
     return len(new_list)
+def sort_on(dict):
+    return dict['count']
 
 def word_instances(text):
     instances_dict = {}
     alphabet = list(string.ascii_lowercase)
     for letter in alphabet:
-        instances_dict[letter] = text.lower().count(letter)
-    return instances_dict
+       instances_dict[letter] = text.lower().count(letter)
+    instances_list = [{"letter":item, "count":instances_dict[item]} for item in instances_dict]
+    instances_list.sort(reverse=True,key=sort_on)
+    return instances_list
+def report(word_count, count_letter_instances):
+    print("---Begin report of books/frankenstein.txt---")
+    print(f"{word_count} words found in the document")
+    print("")
+    for item in count_letter_instances:
+        print(f"The {item["letter"]} character was found {item["count"]} times")
+    print("---End of report---")
 
 
 
